8f4eccc405de747aa84392d238f614f1af3b8bdc
Baby055 <hei.sombiniaina@gmail.com>
2026-08-13 17:50:59 +0300

    feat(GradeRepository) : added with findByStudentId , findByStudentIdAndExamId and findByStudentIdAndExam_CourseId)

---
+++ src/main/java/hei/poja/io/repository/GradeRepository.java
@@ -0,0 +1,16 @@
+package hei.poja.io.repository;
+
+import hei.poja.io.repository.model.JGrade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface GradeRepository extends JpaRepository<JGrade, UUID> {
+    List<JGrade> findByStudentId(UUID studentId);
+
+    Optional<JGrade> findByStudentIdAndExamId(UUID studentId, UUID examId);
+
+    List<JGrade> findByStudentIdAndExam_CourseId(UUID studentId, UUID courseId);
+}
