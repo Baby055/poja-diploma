a219cc7860c18e6cf5d6b161bfe22b9cd79f23c1
Baby055 <hei.sombiniaina@gmail.com>
2026-08-13 17:03:35 +0300

    feat(JExam) : added JExam with course , title , dateExam , coefficient , academicYear and semester

---
+++ src/main/java/hei/poja/io/repository/model/JExam.java
@@ -0,0 +1,32 @@
+package hei.poja.io.repository.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "exam")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JExam {
+    @Id private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private JCourse course;
+
+    private String title;
+
+    private Instant dateExam;
+
+    private BigDecimal coefficient;
+
+    private int academicYear;
+
+    private int semester;
+}
