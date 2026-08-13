3a20b5eb85e1831bc3dcf674052e65af8aa0fa99
Baby055 <hei.sombiniaina@gmail.com>
2026-08-13 20:58:12 +0300

    feat(CourseAssignmentMapper&CourseMapper&GroupMapper&TeacherMapper) : added for the maping

---
+++ src/main/java/hei/poja/io/mapper/GroupMapper.java
@@ -0,0 +1,37 @@
+package hei.poja.io.mapper;
+
+import hei.poja.io.model.Group;
+import hei.poja.io.repository.model.JGroup;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GroupMapper {
+
+    public List<Group> toModel(List<JGroup> jGroups) {
+        return jGroups.stream().map(this::toModel).toList();
+    }
+
+    public Group toModel(JGroup jGroup) {
+        return Group.builder()
+                .id(jGroup.getId())
+                .ref(jGroup.getRef())
+                .track(jGroup.getTrack())
+                .academicYear(jGroup.getAcademicYear())
+                .build();
+    }
+
+    public List<JGroup> toEntity(List<Group> groups) {
+        return groups.stream().map(this::toEntity).toList();
+    }
+
+    public JGroup toEntity(Group group) {
+        return JGroup.builder()
+                .id(group.id())
+                .ref(group.ref())
+                .track(group.track())
+                .academicYear(group.academicYear())
+                .build();
+    }
+}
