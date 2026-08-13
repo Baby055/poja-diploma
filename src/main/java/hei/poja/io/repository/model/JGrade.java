1f62a7d89abe0ddee61602e77f7ba657ec90ebb1
Baby055 <hei.sombiniaina@gmail.com>
2026-08-13 17:09:22 +0300

    feat(JGrade) : added with student , exam , value and lastModifiedAt

---
+++ src/main/java/hei/poja/io/repository/model/JGrade.java
@@ -0,0 +1,30 @@
+package hei.poja.io.repository.model;
+
+import hei.poja.io.model.Student;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "grade")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JGrade {
+    @Id private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private JExam exam;
+
+    private BigDecimal value;
+
+    private Instant lastModifiedAt;
+}
