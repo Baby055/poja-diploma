6f2f00f91ad82ff7b0a56b381145e24d0bbbc1b0
Sombin'ny Aina <hei.sombiniaina@gmail.com>
2026-08-14 17:00:14 +0300

    Feat/security and access controll(#8)
    * feat(AppUserDetails) : added for custom security user mapping)
    
    * feat(AppUserDetailsService) : added to load users by email for authentification
    
    * feat(SecurityConf) : added for Spring Security setup and access control
    
    * style(format.sh)

---
+++ src/main/java/hei/poja/io/security/AppUserDetails.java
@@ -0,0 +1,25 @@
+package hei.poja.io.security;
+
+import hei.poja.io.repository.model.JAppUser;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class AppUserDetails extends User {
+  private final UUID id;
+
+  public AppUserDetails(JAppUser appUser) {
+    super(appUser.getEmail(), appUser.getPasswordHash(), authorities(appUser));
+    this.id = appUser.getId();
+  }
+
+  private static List<GrantedAuthority> authorities(JAppUser appUser) {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + appUser.getRole().name()));
+  }
+
+  public UUID getId() {
+    return id;
+  }
+}
