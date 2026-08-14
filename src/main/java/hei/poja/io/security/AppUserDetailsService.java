cd0b572288236a9ea6c4d56e962a4e6043410732
Baby055 <hei.sombiniaina@gmail.com>
2026-08-14 16:49:00 +0300

    feat(AppUserDetailsService) : added to load users by email for authentification

---
+++ src/main/java/hei/poja/io/security/AppUserDetailsService.java
@@ -0,0 +1,23 @@
+package hei.poja.io.security;
+
+import hei.poja.io.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var appUser =
+                appUserRepository
+                        .findByEmail(email)
+                        .orElseThrow(() -> new UsernameNotFoundException("Unknown user: " + email));
+        return new AppUserDetails(appUser);
+    }
+}
