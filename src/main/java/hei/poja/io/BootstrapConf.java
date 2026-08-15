821f7ff32af07b6007a7f9172e0a54a74c70c1bb
Sombin'ny Aina <hei.sombiniaina@gmail.com>
2026-08-15 12:57:14 +0300

    fix/db.migration : fixed the order (#16)

--- src/main/java/hei/poja/io/BootstrapConf.java
+++ src/main/java/hei/poja/io/BootstrapConf.java
@@ -4,7 +4,6 @@ import hei.poja.io.model.Role;
 import hei.poja.io.repository.AppUserRepository;
 import hei.poja.io.repository.model.JAppUser;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -12,18 +11,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@AllArgsConstructor
 @Slf4j
 public class BootstrapConf implements CommandLineRunner {
 
   private final AppUserRepository appUserRepository;
   private final PasswordEncoder passwordEncoder;
+  private final String bootstrapEmail;
+  private final String bootstrapPassword;
 
-  @Value("${bootstrap.admin.email:admin@hei.school}")
-  private String bootstrapEmail;
-
-  @Value("${bootstrap.admin.password:mot_de_passe_fictif}")
-  private String bootstrapPassword;
+  public BootstrapConf(
+      AppUserRepository appUserRepository,
+      PasswordEncoder passwordEncoder,
+      @Value("${bootstrap.admin.email:admin@hei.school}") String bootstrapEmail,
+      @Value("${bootstrap.admin.password:changeme}") String bootstrapPassword) {
+    this.appUserRepository = appUserRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.bootstrapEmail = bootstrapEmail;
+    this.bootstrapPassword = bootstrapPassword;
+  }
 
   @Override
   public void run(String... args) {
