6cc70d3544afc5b4b22f2fd04e335ecdb84e5aea
Baby055 <hei.sombiniaina@gmail.com>
2026-08-15 09:30:14 +0300

    feat(PromotionsWebController) : added to render promotions view

---
+++ src/main/java/hei/poja/io/endpoint/rest/web/PromotionsWebController.java
@@ -0,0 +1,19 @@
+package hei.poja.io.endpoint.rest.web;
+
+import org.springframework.ui.Model;
+import hei.poja.io.service.GraduateExportService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class PromotionsWebController {
+    private final GraduateExportService graduateExportService;
+
+    @GetMapping("/promotions")
+    public String promotions(Model model) {
+        model.addAttribute("promotions", graduateExportService.promotions());
+        return "promotions";
+    }
+}
